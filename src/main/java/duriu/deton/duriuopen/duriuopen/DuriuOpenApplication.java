0f4ad5fe8288456680a391a44138ede25f0f7243
Nguyen Minh Hoang <ngminhoang@vnpt.vn>
2026-03-17 10:59:17 +0700

    init

---
+++ src/main/java/duriu/deton/duriuopen/duriuopen/DuriuOpenApplication.java
@@ -0,0 +1,13 @@
+package duriu.deton.duriuopen.duriuopen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuriuOpenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuriuOpenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/duriu/deton/duriuopen/duriuopen/DuriuOpenApplicationTests.java
@@ -0,0 +1,13 @@
+package duriu.deton.duriuopen.duriuopen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuriuOpenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
